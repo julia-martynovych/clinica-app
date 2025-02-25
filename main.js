74857e09901e4a1a03a78bd422cc91d6312a441c
Noemi1977 <noemi.casaprima.pendas@gmail.com>
2025-02-25 10:04:44 +0100

    made function search

--- main.js
+++ main.js
@@ -4,59 +4,70 @@ const tableHead = table.outerHTML;
 let editPet = null;
 
 
-async function printAllFilms() {
-    let films = await getAllFilms();
-    printFilms(films)
+async function printAllPets() {
+    let pets = await getAllPets();
+    printPets(pets)
 }
 
 // SEARCH: 
-async function searchFilms() {
+async function searchPets() {
     try {
-        const year = document.getElementById("year").value;
-        const director = document.getElementById("director").value.toLowerCase();
-        const title = document.getElementById("title").value.toLowerCase();
+        const id = document.getElementById("id").value;
+        const species = document.getElementById("species").value.toLowerCase();
+        const name = document.getElementById("name").value.toLowerCase();
+        const sex = document.getElementById("sex").value.toLowerCase();
+        const date_of_birth = document.getElementById("date_of_birth").value.toLowerCase();
+        const owner  = document.getElementById("owner").value.toLowerCase();
+        const date_of_last_visit = document.getElementById("date_of_last_visit").value.toLowerCase();
 
-        let films = await getAllFilms();
+        let pets = await getAllPets();
 
       
-        let filteredFilms = films.filter((film) => {
-            const matchesYear = year ? film.year == year : true;
-            const matchesDirector = director ? film.director.toLowerCase().includes(director) : true;
-            const matchesTitle = title ? film.title.toLowerCase().includes(title) : true;
+        let filteredPets = pets.filter((pet) => {
+            const matchesId = id ? pet.id == id : true;
+            const matchesSpecies = species ? pet.species.includes(species) : true;
+            const matchesName = name ? pet.name.toLowerCase().includes(name) : true;
+            const matchesSex = sex ? pet.sex.includes(sex) : true;
+            const matchesDate_of_birth = date_of_birth ? pet.date_of_birth.includes(date_of_birth) : true;
+            const matchesOwner = owner ? pet.owner.toLowerCase().includes(owner) : true;
+            const matchesDate_of_last_visit = date_of_last_visit ? pet.date_of_last_visit.includes(date_of_last_visit) : true;
             
-            return matchesYear && matchesDirector && matchesTitle;
+            return matchesId && matchesSpecies && matchesName && matchesSex && matchesDate_of_birth && matchesOwner && matchesDate_of_last_visit;
         });
 
       
-        printFilms(filteredFilms);
+        printPets(filteredPets);
 
     } catch (error) {
         console.log("Error:", error);
     }
 }
 
-function printFilms(films) {
+function printPets(pets) {
     table.innerHTML = tableHead;
 
     
-    if (films.length === 0) {
-        table.insertAdjacentHTML("beforeend", "<tr><td colspan='4'>No films found</td></tr>");
+    if (pets.length === 0) {
+        table.insertAdjacentHTML("beforeend", "<tr><td colspan='4'>No se encuentran animales</td></tr>");
         return;
     }
 
-    films.forEach((film) => {
+    pets.forEach((pet) => {
         const row = document.createElement("tr");
         row.innerHTML = `
-            <td>${film.id}</td>
-            <td>${film.title}</td>
-            <td>${film.year}</td>
-            <td>${film.director}</td>
+            <td>${pet.id}</td>
+            <td>${pet.species}</td>
+            <td>${pet.name}</td>
+            <td>${pet.sex}</td>
+            <td>${pet.date_of_birth}</td>
+            <td>${pet.owner}</td>
+            <td>${pet.date_of_last_visit}</td>
             <td><button class="delete-btn"><i class="fa-solid fa-trash"></i></button></td>
             <td><button class="edit-btn"><i class="fa-solid fa-pen-to-square"></i></button></td>
         `;
-        row.querySelector(".delete-btn").addEventListener("click", (event) => deleteFilm(film.id, event));
+        row.querySelector(".delete-btn").addEventListener("click", (event) => deleteFilm(pet.id, event));
         row.querySelector(".edit-btn").addEventListener("click", () => {
-            editFilm = film;
+            editPet = pet;
             
          });
         table.appendChild(row);
