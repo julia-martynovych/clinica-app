a96476dba0a8461af9226dc209e797be3ef92cf4
Noemi1977 <noemi.casaprima.pendas@gmail.com>
2025-02-25 09:45:35 +0100

    introduce first variables in JS

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hola")
+const API_URL = "http://localhost:3000/pets";
+const table = document.getElementById("pets-table");
+const tableHead = table.outerHTML;
+let editPet = null;
+
+
+async function printAllFilms() {
+    let films = await getAllFilms();
+    printFilms(films)
+}
